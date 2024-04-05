75e7b049f6cf9511e8b7a7e9ad4ddbd1149ee180
poulkalff <poulkalff@gmail.com>
2024-04-05 18:36:12 +0200

    created init second map

--- mapGenerator.py
+++ mapGenerator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-height = 46
+height = 6
 width = 12
 name = 'test'
 no = 0
@@ -10,15 +10,19 @@ player = 'test'
 print('{')
 print('\t"mapName" :\t"' + name + '",')
 print('\t"mapNo" :\t' + str(no) + ',')
-print('\t"player" : \t"' + player + '",')
+print('\t"player" :\t"' + player + '",')
 
-print('\t"tiles" :{')
-for x in range(height) - 1:
-    print('\t\t\t"line' + str(x + 1) + '":\t[', end="")
+print('\t"tiles"\t:{')
+for x in range(height + 1):
+    print('\t\t\t\t"line' + str(x + 1) + '":\t[', end="")
     for y in range(width):
-        print('["grass","",""]', end="")
+        print('["g","",""]', end="")
         if y < width - 1:
             print(', ', end="")
-    print("],")
+    print("]", end="")
+    if x < height:
+        print(',')
+    else:
+        print()
 print('\t\t}')
 print('}')
